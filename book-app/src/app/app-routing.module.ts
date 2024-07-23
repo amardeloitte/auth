930a985e94ff4825f3b445b82a4b9792dc6ede22
Ram <amaram@deloitte.com>
2024-07-23 16:34:07 +0530

    registration done

--- book-app/src/app/app-routing.module.ts
+++ book-app/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BookListComponent } from './book-list/book-list.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "/dashboard", pathMatch: "full"},
+  {path: "dashboard", component: DashboardComponent},
+  {path: "book-list", component: BookListComponent},
+  {path: "register", component: RegisterComponent},
+  {path: "login", component: LoginComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- book-app/src/app/register/register.component.ts
+++ book-app/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+
+  registerUserData = {email: "", password: ""};
+
+  constructor(private authService: AuthService){}
+
+  registerUser() {
+    this.authService.registerUser(this.registerUserData).subscribe({
+      next: res => {
+        console.log(res)
+      },
+      error: err => {
+        console.log(err);
+      }
+    })
+  }
 
 }
